2d6cc4b6af7679be0d0ee77a0f891663fd2076ea
Joe Mazzone <jmazzone@daviestech.org>
2020-11-05 04:18:18 +0000

    updates

--- MathClassesExample.java
+++ MathClassesExample.java
@@ -1,7 +1,22 @@
-
+import java.util.Scanner;
+import java.lang.Math;
 
 class MathClassesExample {
     public static void main(String[] args) {
         
+    Scanner scan = new Scanner (System.in);
+   
+    System.out.println("Please enter 2 ints: ");
+    int x = scan.nextInt();
+    int y = scan.nextInt();
+    
+    System.out.println("Please enter 2 doubles: ");
+    double a = scan.nextDouble();
+    double b = scan.nextDouble();
+    
+    System.out.println("Answers: ");
+    System.out.println(Math.abs(x - y));
+    System.out.println(Math.abs(a - b));
+
     }
 }
